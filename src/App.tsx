801ace2bd938a9852bf3f014e8ec56da5f9d5ed4
Tyler Wright <tyler.david.wright@gmail.com>
2020-09-28 13:06:37 +0100

    Improved construction page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import React from "react";
 
 export default function App() {
     return (
-        <div>{ "This page is unavailable at the moment, come back later!" }</div>
+        <div className="d-flex flex-column mt-5">
+            <img className="align-self-center" src="res/logo.png" alt=""/> 
+            <h2 className="text-center">{ 
+                "The Fluxanoia website is undergoing some changes, come back later to see the new look!" 
+            }</h2>
+        </div>
+        
     );
 }
