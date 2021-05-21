cb529cf9b81e850c540742e75826f1f3dbb69d2b
Tyler Wright <tyler.david.wright@gmail.com>
2021-05-22 00:20:45 +0100

    Fixed links

--- src/pages/pages.tsx
+++ src/pages/pages.tsx
@@ -29,47 +29,46 @@ type PageProps = {
 };
 export default class Page {
     private component : React.ComponentType<any> | undefined;
-    private props : PageProps;
+    private _props : PageProps;
 
     public constructor(component : React.ComponentType<any>, props : PageProps) {
         this.component = component;
-        props.link = props.link ?? `/` + props.name.toLowerCase();
-        this.props = props;
+        this._props = props;
     }
 
     public getButton(forceHover : boolean) {
         const links = {
-            to: this.props.local ? this.props.link : undefined,
-            href: this.props.local ? undefined : this.props.link,
+            [this.isLocal() ? `to` : `href`]: this.getLink(),
         }
         return (
-            <ButtonContainer key={ "link-" + this.props.name }>
+            <ButtonContainer key={ `link-${this.getName()}` }>
                 <Button {...links} forceHover={ forceHover }>
-                    { this.props.name }
+                    { this.getName() }
                 </Button>
             </ButtonContainer>
         );
     }
     public getRoute() {
-        const key = "route-" + this.props.name;
-        if (this.props.notFound) return <Route key={ key } component={ this.component } />;
+        const key = `route-${this.getName()}`;
+        if (this.isNotFound()) return <Route key={ key } component={ this.component } />;
         return <Route
             key={ key }
-            path={ this.props.link }
+            path={ this.getLink() }
             exact
             component={ this.component } />;
     }
     
-    public hasLink(link : string) { return link === this.props.link; }
+    public hasLink(link : string) { return link === this.getLink(); }
 
     public getComponent() { return this.component; }
-    public getColour() { return this.props.accentColour ?? defaultBgColour };
-    public getName() { return this.props.name; }
-    public isLocal() { return this.props.local ?? true; }
-    public isOnNavbar() { return this.props.onNavbar ?? false; }
-    public isHome() { return this.props.home ?? false; }
-    public isNotFound() { return this.props.notFound ?? false; }
-    public isIndependent() { return this.props.independent ?? false; }
+    public getColour() { return this._props.accentColour ?? defaultBgColour };
+    public getName() { return this._props.name; }
+    public getLink() { return this._props.link ?? `/${this.getName().toLowerCase()}`; }
+    public isLocal() { return this._props.local ?? true; }
+    public isOnNavbar() { return this._props.onNavbar ?? false; }
+    public isHome() { return this._props.home ?? false; }
+    public isNotFound() { return this._props.notFound ?? false; }
+    public isIndependent() { return this._props.independent ?? false; }
 };
 
 const ButtonContainer = styled.div`
